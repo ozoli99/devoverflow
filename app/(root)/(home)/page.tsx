7d742452228bd48de59ef8625a81cddf32682aab
zoltan.orosz <zoltan.orosz@leopoly.com>
2023-10-28 13:02:29 +0200

    Add parameter types and question actions

--- app/(root)/(home)/page.tsx
+++ app/(root)/(home)/page.tsx
@@ -6,6 +6,7 @@ import { HomePageFilters } from "@/constants/filters";
 import HomeFilters from "@/components/home/HomeFilters";
 import NoResult from "@/components/shared/NoResult";
 import QuestionCard from "@/components/cards/QuestionCard";
+import { getQuestions } from "@/lib/actions/question.action";
 
 const questions = [
     {
@@ -41,7 +42,9 @@ const questions = [
     },
 ];
 
-export default function Home() {
+export default async function Home() {
+    const result = await getQuestions({});
+
     return (
         <>
             <div className="flex w-full flex-col-reverse justify-between gap-4 sm:flex-row sm:items-center">
@@ -74,8 +77,8 @@ export default function Home() {
             <HomeFilters />
 
             <div className="mt-10 flex w-full flex-col gap-6">
-                {questions.length > 0 ? (
-                    questions.map((question) => (
+                {result.questions.length > 0 ? (
+                    result.questions.map((question) => (
                         <QuestionCard
                             key={question._id}
                             _id={question._id}
--- components/forms/Question.tsx
+++ components/forms/Question.tsx
@@ -20,12 +20,19 @@ import { Editor } from "@tinymce/tinymce-react";
 import { Badge } from "../ui/badge";
 import Image from "next/image";
 import { createQuestion } from "@/lib/actions/question.action";
+import { useRouter, usePathname } from "next/navigation";
 
 const type: any = "create";
 
-const Question = () => {
+interface Props {
+    mongoUserId: string;
+}
+
+const Question = ({ mongoUserId }: Props) => {
     const editorRef = useRef(null);
     const [isSubmitting, setIsSubmitting] = useState(false);
+    const router = useRouter();
+    const pathname = usePathname();
 
     const form = useForm<z.infer<typeof QuestionsSchema>>({
         resolver: zodResolver(QuestionsSchema),
@@ -42,8 +49,15 @@ const Question = () => {
         try {
             // make an async call to your API -> create a question
             // contain all form data
-            await createQuestion({});
+            await createQuestion({
+                title: values.title,
+                content: values.explanation,
+                tags: values.tags,
+                author: JSON.parse(mongoUserId),
+                path: pathname,
+            });
             // navigate to home page
+            router.push("/");
         } catch (error) {
         } finally {
             setIsSubmitting(false);
