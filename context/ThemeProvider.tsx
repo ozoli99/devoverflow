bb7a701a962229ccd39155e780d142ae6a823174
zoltan.orosz <zoltan.orosz@leopoly.com>
2023-11-07 12:35:45 +0100

    Add a console.log

--- context/ThemeProvider.tsx
+++ context/ThemeProvider.tsx
@@ -30,6 +30,8 @@ export function ThemeProvider({ children }: { children: React.ReactNode }) {
         handleThemeChange();
     }, [mode]);
 
+    console.log("MODE, ", mode);
+
     return (
         <ThemeContext.Provider value={{ mode, setMode }}>
             {children}
