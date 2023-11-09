009cb01abc18476c43ab3ca80ef3ea5c554cf8ed
zoltan.orosz <zoltan.orosz@leopoly.com>
2023-11-09 14:31:34 +0100

    Handle voting logic

--- app/(root)/question/[id]/page.tsx
+++ app/(root)/question/[id]/page.tsx
@@ -44,7 +44,7 @@ const Page = async ({ params, searchParams }) => {
                     </Link>
                     <div className="flex justify-end">
                         <Votes
-                            type="question"
+                            type="Question"
                             itemId={JSON.stringify(result._id)}
                             userId={JSON.stringify(mongoUser._id)}
                             upvotes={result.upvotes.length}
@@ -100,7 +100,7 @@ const Page = async ({ params, searchParams }) => {
 
             <AllAnswers
                 questionId={result._id}
-                userId={JSON.stringify(mongoUser._id)}
+                userId={mongoUser._id}
                 totalAnswers={result.answers.length}
             />
 
--- components/shared/Votes.tsx
+++ components/shared/Votes.tsx
@@ -1,7 +1,13 @@
 "use client";
 
+import { downvoteAnswer, upvoteAnswer } from "@/lib/actions/answer.action";
+import {
+    downvoteQuestion,
+    upvoteQuestion,
+} from "@/lib/actions/question.action";
 import { formatAndDivideNumber } from "@/lib/utils";
 import Image from "next/image";
+import { usePathname, useRouter } from "next/navigation";
 import React from "react";
 
 interface Props {
@@ -25,9 +31,61 @@ const Votes = ({
     hasdownVoted,
     hasSaved,
 }: Props) => {
+    const pathname = usePathname();
+    const router = useRouter();
+
     const handleSave = () => {};
 
-    const handleVote = (action: string) => {};
+    const handleVote = async (action: string) => {
+        if (!userId) {
+            return;
+        }
+
+        if (action === "upvote") {
+            if (type === "Question") {
+                await upvoteQuestion({
+                    questionId: JSON.parse(itemId),
+                    userId: JSON.parse(userId),
+                    hasupVoted,
+                    hasdownVoted,
+                    path: pathname,
+                });
+            } else if (type === "Answer") {
+                await upvoteAnswer({
+                    answerId: JSON.parse(itemId),
+                    userId: JSON.parse(userId),
+                    hasupVoted,
+                    hasdownVoted,
+                    path: pathname,
+                });
+            }
+
+            // TODO: Show a toast
+            return;
+        }
+
+        if (action === "downvote") {
+            if (type === "Question") {
+                await downvoteQuestion({
+                    questionId: JSON.parse(itemId),
+                    userId: JSON.parse(userId),
+                    hasupVoted,
+                    hasdownVoted,
+                    path: pathname,
+                });
+            } else if (type === "Answer") {
+                await downvoteAnswer({
+                    answerId: JSON.parse(itemId),
+                    userId: JSON.parse(userId),
+                    hasupVoted,
+                    hasdownVoted,
+                    path: pathname,
+                });
+            }
+
+            // TODO: Show a toast
+        }
+    };
 
     return (
         <div className="flex gap-5">
@@ -74,18 +132,20 @@ const Votes = ({
                     </div>
                 </div>
             </div>
-            <Image
-                src={
-                    hasSaved
-                        ? "/assets/icons/star-filled.svg"
-                        : "/assets/icons/star-red.svg"
-                }
-                alt="star"
-                width={18}
-                height={18}
-                className="cursor-pointer"
-                onClick={() => handleSave()}
-            />
+            {type === "Question" && (
+                <Image
+                    src={
+                        hasSaved
+                            ? "/assets/icons/star-filled.svg"
+                            : "/assets/icons/star-red.svg"
+                    }
+                    alt="star"
+                    width={18}
+                    height={18}
+                    className="cursor-pointer"
+                    onClick={() => handleSave()}
+                />
+            )}
         </div>
     );
 };
