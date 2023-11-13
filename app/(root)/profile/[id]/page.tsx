0acbc1b0b41bccb077b882161b2bf98891920d9d
zoltan.orosz <zoltan.orosz@leopoly.com>
2023-11-13 14:26:38 +0100

    Create User Stats UI

--- app/(root)/profile/[id]/page.tsx
+++ app/(root)/profile/[id]/page.tsx
@@ -1,4 +1,5 @@
 import ProfileLink from "@/components/shared/ProfileLink";
+import Stats from "@/components/shared/Stats";
 import { Button } from "@/components/ui/button";
 import { Tabs, TabsContent, TabsList, TabsTrigger } from "@/components/ui/tabs";
 import { getUserInfo } from "@/lib/actions/user.action";
@@ -70,7 +71,10 @@ const Page = async ({ params, searchParams }: URLProps) => {
                     </SignedIn>
                 </div>
             </div>
-            Stats
+            <Stats
+                totalQuestions={userInfo.totalQuestions}
+                totalAnswers={userInfo.totalAnswers}
+            />
             <div className="mt-10 flex gap-10">
                 <Tabs defaultValue="top-posts" className="flex-1">
                     <TabsList className="background-light800_dark400 min-h-[42px] p-1">
@@ -81,8 +85,8 @@ const Page = async ({ params, searchParams }: URLProps) => {
                             Answers
                         </TabsTrigger>
                     </TabsList>
-                    <TabsContent value="top-posts">POSTS</TabsContent>
-                    <TabsContent value="answers">ANSWERS</TabsContent>
+                    <TabsContent value="top-posts">QuestionTab</TabsContent>
+                    <TabsContent value="answers">AnswersTab</TabsContent>
                 </Tabs>
             </div>
         </>
