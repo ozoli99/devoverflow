7dfbec7fc8d4c7538fb388c9b567107b9872305d
zoltan.orosz <zoltan.orosz@leopoly.com>
2023-11-16 14:17:41 +0100

    Manage filter state

--- components/home/HomeFilters.tsx
+++ components/home/HomeFilters.tsx
@@ -1,11 +1,38 @@
 "use client";
 
-import React from "react";
+import React, { useState } from "react";
 import { HomePageFilters } from "@/constants/filters";
 import { Button } from "../ui/button";
+import { useSearchParams, useRouter } from "next/navigation";
+import { formUrlQuery } from "@/lib/utils";
 
 const HomeFilters = () => {
-    const active = "frequent";
+    const searchParams = useSearchParams();
+    const router = useRouter();
+
+    const [active, setActive] = useState("");
+
+    const handleTypeClick = (item: string) => {
+        if (active === item) {
+            setActive("");
+
+            const newUrl = formUrlQuery({
+                params: searchParams.toString(),
+                key: "filter",
+                value: null,
+            });
+            router.push(newUrl, { scroll: false });
+        } else {
+            setActive(item);
+
+            const newUrl = formUrlQuery({
+                params: searchParams.toString(),
+                key: "filter",
+                value: item.toLowerCase(),
+            });
+            router.push(newUrl, { scroll: false });
+        }
+    };
 
     return (
         <div className="mt-10 flex-wrap gap-3 md:flex">
@@ -13,6 +40,7 @@ const HomeFilters = () => {
                 <Button
                     key={item.value}
                     onClick={() => {}}
+                    onClickCapture={() => handleTypeClick(item.value)}
                     className={`body-medium rounded-lg px-6 py-3 capitalize shadow-none ${
                         active === item.value
                             ? "bg-primary-100 text-primary-500"
