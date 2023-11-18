1b1cfbe63508015b3936b0a8d1ec8fd5bc403c60
zoltan.orosz <zoltan.orosz@leopoly.com>
2023-11-18 15:43:11 +0100

    Implement the Global Search action

--- components/shared/search/GlobalResult.tsx
+++ components/shared/search/GlobalResult.tsx
@@ -5,6 +5,7 @@ import Image from "next/image";
 import GlobalFilters from "./GlobalFilters";
 import React, { useEffect, useState } from "react";
 import { ReloadIcon } from "@radix-ui/react-icons";
+import { globalSearch } from "@/lib/actions/general.action";
 import { useSearchParams } from "next/navigation";
 
 const GlobalResult = () => {
@@ -26,6 +27,8 @@ const GlobalResult = () => {
             setIsLoading(true);
 
             try {
+                const res = await globalSearch({ query: global, type });
+                setResult(JSON.parse(res));
             } catch (error) {
                 console.log(error);
                 throw error;
@@ -33,10 +36,25 @@ const GlobalResult = () => {
                 setIsLoading(false);
             }
         };
+
+        if (global) {
+            fetchResult();
+        }
     }, [global, type]);
 
     const renderLink = (type: string, id: string) => {
-        return "/";
+        switch (type) {
+            case "question":
+                return `/question/${id}`;
+            case "answer":
+                return `/question/${id}`;
+            case "user":
+                return `/profile/${id}`;
+            case "tag":
+                return `/tags/${id}`;
+            default:
+                return "/";
+        }
     };
 
     return (
@@ -59,7 +77,7 @@ const GlobalResult = () => {
                         {result.length > 0 ? (
                             result.map((item: any, index: number) => (
                                 <Link
-                                    href={renderLink("type", "id")}
+                                    href={renderLink(item.type, item.id)}
                                     key={item.type + item.id + index}
                                     className="flex w-full cursor-pointer items-start gap-3 px-5 py-2.5 hover:bg-light-700/50 dark:bg-dark-500/50"
                                 >
--- components/shared/search/GlobalSearch.tsx
+++ components/shared/search/GlobalSearch.tsx
@@ -2,7 +2,7 @@
 
 import Image from "next/image";
 import GlobalResult from "./GlobalResult";
-import React, { useEffect, useState } from "react";
+import React, { useEffect, useRef, useState } from "react";
 import { Input } from "@/components/ui/input";
 import { formUrlQuery, removeKeysFromQuery } from "@/lib/utils";
 import { usePathname, useRouter, useSearchParams } from "next/navigation";
@@ -11,11 +11,33 @@ const GlobalSearch = () => {
     const router = useRouter();
     const pathname = usePathname();
     const searchParams = useSearchParams();
+    const searchContainerRef = useRef(null);
 
     const query = searchParams.get("q");
 
     const [search, setSearch] = useState(query || "");
     const [isOpen, setIsOpen] = useState(false);
+
+    useEffect(() => {
+        const handleOutsideClick = (event: any) => {
+            if (
+                searchContainerRef.current &&
+                // @ts-ignore
+                !searchContainerRef.current.contains(event.target)
+            ) {
+                setIsOpen(false);
+                setSearch("");
+            }
+        };
+
+        setIsOpen(false);
+
+        document.addEventListener("click", handleOutsideClick);
+
+        return () => {
+            document.removeEventListener("click", handleOutsideClick);
+        };
+    }, [pathname]);
 
     useEffect(() => {
         const delayDebounceFn = setTimeout(() => {
@@ -41,7 +63,10 @@ const GlobalSearch = () => {
     }, [search, router, pathname, searchParams, query]);
 
     return (
-        <div className="relative w-full max-w-[600px] max-lg:hidden">
+        <div
+            className="relative w-full max-w-[600px] max-lg:hidden"
+            ref={searchContainerRef}
+        >
             <div className="background-light800_darkgradient relative flex min-h-[56px] grow items-center gap-1 rounded-xl px-4">
                 <Image
                     src="/assets/icons/search.svg"
